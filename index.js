dd3d5636f05e4e8e16b5aed916432c4a3094c5d1
TomatoChips <45629362+PaulisX@users.noreply.github.com>
2025-01-08 17:20:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+let characteristic;
+let sliderA = document.getElementById('motA');
+let sliderB = document.getElementById('motB');
+
 async function connectToBluetoothDevice() {
+   sliderA = document.getElementById('motA');
+   sliderB = document.getElementById('motB');
    try {
        // Request the Bluetooth device
       const device = await navigator.bluetooth.requestDevice({
@@ -21,26 +27,17 @@ async function connectToBluetoothDevice() {
       const service = await server.getPrimaryService('0000ffe0-0000-1000-8000-00805f9b34fb');
       console.log('Battery service accessed.');
 
-      const characteristic = await service.getCharacteristic('0000ffe1-0000-1000-8000-00805f9b34fb');
-      let sliderA = document.getElementById('motA');
-      let sliderB = document.getElementById('motB');
+      characteristic = await service.getCharacteristic('0000ffe1-0000-1000-8000-00805f9b34fb');
+      const resetEnergyExpended = Int8Array.of(0,0);
+      await characteristic.writeValue(resetEnergyExpended);
+      await characteristic.writeValue(resetEnergyExpended);
       while(true){
          let motA = sliderA.value;
          let motB = sliderB.value;
          const resetEnergyExpended = Int8Array.of(motA,motB);
-         await characteristic.writeValue(resetEnergyExpended);
+         await characteristic.writeValue(resetEnergyExpended); 
       }      
-      
-
-      // // Access a characteristic (e.g., Battery Level)
-      // const value = await characteristic.readValue();
-      // const batteryLevel = value.getUint8(0);
-      // console.log('Battery level:', batteryLevel + '%');
    } catch (error) {
       console.error('Error:', error);
    }
 }
-
-// Call the function
-
-// connectToBluetoothDevice();
